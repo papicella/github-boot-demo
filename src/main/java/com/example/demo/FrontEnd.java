f972ac8d2479daa1ab5fb42c0d68865102ef42f5
Administrator <pas.apicella@gmail.com>
2020-06-17 19:29:01 +1000

    Initial commit

---
+++ src/main/java/com/example/demo/FrontEnd.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@Slf4j
+public class FrontEnd {
+    @GetMapping("/")
+    public String index () {
+        log.info("An INFO Message");
+
+        return new Date().toString();
+    }
+}
